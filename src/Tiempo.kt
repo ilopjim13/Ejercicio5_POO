a2f22af5b661aadbc903ad3e5de3e0ae1ab34e48
ilopjim13 <ljivan@iesrafaelalberti.es>
2024-01-30 19:50:56 +0100

    metodos nuevos empezados en clase

--- src/Tiempo.kt
+++ src/Tiempo.kt
@@ -38,6 +38,36 @@ class Tiempo (hora:Int) {
         return "${this.hora}h ${this.minutos}m ${this.segundos}s"
     }
 
+    fun incrementar(t:Tiempo) : Boolean {
+        val h = t.hora
+        val m = t.minutos
+        val s = t.segundos
+        if (s > 59) {
+            this.minutos += s/60
+            this.segundos %= 60
+        }
+
+        if (m > 59) {
+            this.hora += m/60
+            this.minutos %= 60
+        }
+
+        return if (this.hora + h > 23) false
+
+        else{
+            this.hora += h
+            return true
+        }
+    }
+
+    //fun decrementar(t:Tiempo): Boolean {
+    //}
+
+    fun comparar(t:Tiempo):Int {
+        if (this.hora == t.hora && this.minutos == t.minutos && this.segundos == t.segundos) return 0
+        return 5
+    }
+
 
 
 
