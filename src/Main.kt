11bb52f6e1740f2c658b3a7b191185c1ad0b00c1
ivanjim13 <ivangorrion57@gmail.com>
2024-02-05 00:32:44 +0100

    Terminado

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,179 @@
 
 fun main() {
-    val hora1 = Tiempo(1)
-    print("Introduce la hora: ")
-    hora1.hora = readln().toInt()
-    print("Introduce los minutos: ")
-    hora1.setMinutos(readln())
-    print("Introduce los segundos: ")
-    hora1.setSegundos(readln())
+    var t:Tiempo
+    var h: Int = 0
+    var m: Int = 0
+    var s: Int = 0
+    try {
+        print("Introduce hora: ")
+        h = readln().toInt()
+        print("Introduce minutos: ")
+        m = readln().toInt()
+        print("Introduce segundos: ")
+        s = readln().toInt()
+    } catch (e:NumberFormatException) {
+        println("Error")
+    }
+    catch (e:IllegalArgumentException) {
+        println("Los minutos y los segundos deben ser menor a 59")
+    }
+    val hora = Tiempo(h,m,s)
+    println(hora)
 
-    println(hora1.toString())
 
-    val hora2 = Tiempo(1,2)
-    println(hora2.toString())
+    try {
+        print("Introduce hora: ")
+        h = readln().toInt()
+        print("Introduce minutos: ")
+        m = readln().toInt()
+        print("Introduce segundos: ")
+        s = readln().toInt()
+    } catch (e:NumberFormatException) {
+        println("Error")
+    }
+    catch (e:IllegalArgumentException) {
+        println("Los minutos y los segundos deben ser menor a 59")
+    }
+    t = Tiempo(h,m,s)
+    if (hora.incrementar(t)) println("La hora incrementada: $hora")
+    else println("**ERROR** La hora incrementada se pasa de las 23:59:59")
 
-    val hora3 = Tiempo(1,2,3)
-    println(hora3.toString())
+
+
+//    try {
+//        print("Introduce hora: ")
+//        h = readln().toInt()
+//        print("Introduce minutos: ")
+//        m = readln().toInt()
+//        print("Introduce segundos: ")
+//        s = readln().toInt()
+//    } catch (e:NumberFormatException) {
+//        println("Error")
+//    }
+//    catch (e:IllegalArgumentException) {
+//        println("Los minutos y los segundos deben ser menor a 59")
+//    }
+//    t = Tiempo(h,m,s)
+    if (hora.decrementar(t)) println("La hora decrementada es de $hora")
+    else println("**ERROR** La hora decrementada se pasa de las 00:00:00")
+
+
+//    try {
+//        print("Introduce hora: ")
+//        h = readln().toInt()
+//        print("Introduce minutos: ")
+//        m = readln().toInt()
+//        print("Introduce segundos: ")
+//        s = readln().toInt()
+//    } catch (e:NumberFormatException) {
+//        println("Error")
+//    }
+//    catch (e:IllegalArgumentException) {
+//        println("Los minutos y los segundos deben ser menor a 59")
+//    }
+//    t = Tiempo(h,m,s)
+    if (hora.comparar(t) == 0) println("Las horas son iguales")
+    else if (hora.comparar(t) == 1) println("$hora es mayor que $t")
+    else println("$hora es menor que $t")
+
+
+
+    val copia1 = hora.copiar()
+    println("Esta es la hora $hora y esta su copia $copia1")
+
+
+
+//    try {
+//        print("Introduce hora: ")
+//        h = readln().toInt()
+//        print("Introduce minutos: ")
+//        m = readln().toInt()
+//        print("Introduce segundos: ")
+//        s = readln().toInt()
+//    } catch (e:NumberFormatException) {
+//        println("Error")
+//    }
+//    catch (e:IllegalArgumentException) {
+//        println("Los minutos y los segundos deben ser menor a 59")
+//    }
+//    t = Tiempo(h,m,s)
+    val copia2 = hora.copiar(t)
+    println("Esta es la hora $hora y esta su copia $copia2")
+
+
+//    try {
+//        print("Introduce hora: ")
+//        h = readln().toInt()
+//        print("Introduce minutos: ")
+//        m = readln().toInt()
+//        print("Introduce segundos: ")
+//        s = readln().toInt()
+//    } catch (e:NumberFormatException) {
+//        println("Error")
+//    }
+//    catch (e:IllegalArgumentException) {
+//        println("Los minutos y los segundos deben ser menor a 59")
+//    }
+//    t = Tiempo(h,m,s)
+
+    val sumada = hora.sumar(t)
+    println(sumada)
+
+
+
+//    try {
+//        print("Introduce hora: ")
+//        h = readln().toInt()
+//        print("Introduce minutos: ")
+//        m = readln().toInt()
+//        print("Introduce segundos: ")
+//        s = readln().toInt()
+//    } catch (e:NumberFormatException) {
+//        println("Error")
+//    }
+//    catch (e:IllegalArgumentException) {
+//        println("Los minutos y los segundos deben ser menor a 59")
+//    }
+//    t = Tiempo(h,m,s)
+
+    val restada = hora.restar(t)
+    println(restada)
+
+
+//    try {
+//        print("Introduce hora: ")
+//        h = readln().toInt()
+//        print("Introduce minutos: ")
+//        m = readln().toInt()
+//        print("Introduce segundos: ")
+//        s = readln().toInt()
+//    } catch (e:NumberFormatException) {
+//        println("Error")
+//    }
+//    catch (e:IllegalArgumentException) {
+//        println("Los minutos y los segundos deben ser menor a 59")
+//    }
+//    t = Tiempo(h,m,s)
+
+    if (hora.esMayorQue(t)) println("$hora es mayor que $t")
+    else println("$hora no es mayor que $t")
+
+
+//    try {
+//        print("Introduce hora: ")
+//        h = readln().toInt()
+//        print("Introduce minutos: ")
+//        m = readln().toInt()
+//        print("Introduce segundos: ")
+//        s = readln().toInt()
+//    } catch (e:NumberFormatException) {
+//        println("Error")
+//    }
+//    catch (e:IllegalArgumentException) {
+//        println("Los minutos y los segundos deben ser menor a 59")
+//    }
+//    t = Tiempo(h,m,s)
+
+    if (hora.esMenorQue(t)) println("$hora es menor que $t")
+    else println("$hora no es menor que $t")
 }
--- src/Tiempo.kt
+++ src/Tiempo.kt
@@ -1,11 +1,11 @@
-class Tiempo (hora:Int) {
-    var hora:Int = hora
-    private var minutos:Int = 0
+class Tiempo (var hora:Int, minutos: Int = 0, segundos: Int = 0) {
+
+    private var minutos:Int = minutos
         set(value) {
             require(value in 0..59)
             field = value
         }
-    private var segundos:Int = 0
+    private var segundos:Int = segundos
         set(value) {
             require(value in 0..59)
             field = value
@@ -25,50 +25,140 @@ class Tiempo (hora:Int) {
         }
     }
 
-    constructor(hora: Int, minutos: Int) : this(hora) {
-        this.minutos = minutos
-    }
-
-    constructor(hora: Int, minutos: Int, segundos: Int) : this(hora) {
-        this.minutos = minutos
-        this.segundos = segundos
-    }
-
+    /**
+     * Imprime la informacion de la hora
+     * @return String con la informacion de la hora
+     */
     override fun toString(): String {
         return "${this.hora}h ${this.minutos}m ${this.segundos}s"
     }
 
+    /**
+     * Incrementa la hora y retorna true si la hora no pasa de 23:59:59 o false si se pasa
+     * @param t :Tiempo tiempo que va a incrementar
+     * @return Boolean true si la hora pasa no se pasa de 23:59:59 y false si se pasa
+     */
     fun incrementar(t:Tiempo) : Boolean {
+        val total = (this.hora * 3600) + (this.minutos * 60) + this.segundos
+        val total2 = (t.hora * 3600) + (t.minutos * 60) + t.segundos
+
+        if (total + total2 <= 86399) {
+            val totalFinal = total + total2
+            this.hora = totalFinal/3600
+            this.minutos = (totalFinal % 3600) / 60
+            this.segundos = totalFinal % 60
+            return true
+        } else return false
+    }
+
+    /**
+     * Decrementa la hora y retorna true si la hora no pasa de 00:00:00 o false si se pasa
+     * @param t :Tiempo tiempo que va a decrementar
+     * @return Boolean true si la hora pasa no se pasa de 00:00:00 y false si se pasa
+     */
+    fun decrementar(t:Tiempo): Boolean {
+        val total = (this.hora * 3600) + (this.minutos * 60) + this.segundos
+        val total2 = (t.hora * 3600) + (t.minutos * 60) + t.segundos
+
+        if (total - total2 > 0) {
+            val totalFinal = total - total2
+            this.hora = totalFinal/3600
+            this.minutos = (totalFinal % 3600) / 60
+            this.segundos = totalFinal % 60
+            return true
+        }
+        else return false
+    }
+
+    /**
+     * Compara si las dos horas son iguales o cual es mayor
+     * @param t :Tiempo tiempo que va a comparar
+     * @return Int numero correspondiente a si es menor, mayor o igual
+     */
+    fun comparar(t:Tiempo):Int {
+        val segundos1 = (this.hora * 3600) + (this.minutos * 60) + this.segundos
+        val segundos2 = (t.hora * 3600) + (t.minutos * 60) + t.segundos
+        return if (segundos1 == segundos2) 0
+        else if (segundos1 > segundos2) 1
+        else -1
+    }
+
+
+    /**
+     * Copia la hora principal y devuelve la copia
+     * @return Tiempo retorna la hora copiada
+     */
+    fun copiar():Tiempo {
+        val h = this.hora
+        val m = this.minutos
+        val s = this.segundos
+        return Tiempo(h,m,s)
+    }
+
+    /**
+     * Copia la hora que se le pasa y devuelve una copia
+     * @param t :Tiempo tiempo que se va a copiar
+     * @return Tiempo retorna la hora copiada
+     */
+    fun copiar(t:Tiempo) :Tiempo {
         val h = t.hora
         val m = t.minutos
         val s = t.segundos
-        if (s > 59) {
-            this.minutos += s/60
-            this.segundos %= 60
-        }
-
-        if (m > 59) {
-            this.hora += m/60
-            this.minutos %= 60
-        }
-
-        return if (this.hora + h > 23) false
-
-        else{
-            this.hora += h
-            return true
-        }
+        return Tiempo(h, m, s)
     }
 
-    //fun decrementar(t:Tiempo): Boolean {
-    //}
+    /**
+     * Suma las dos horas y devuelve la hora sumada o un nulo si se pasa de 23:59:59
+     * @param t :Tiempo tiempo que se va a sumar
+     * @return Tiempo? devuelve un objeto Tiempo o un nulo
+     */
+    fun sumar(t:Tiempo) :Tiempo? {
+        val total = (this.hora * 3600) + (this.minutos * 60) + this.segundos
+        val total2 = (t.hora * 3600) + (t.minutos * 60) + t.segundos
 
-    fun comparar(t:Tiempo):Int {
-        if (this.hora == t.hora && this.minutos == t.minutos && this.segundos == t.segundos) return 0
-        return 5
+        if (total + total2 <= 86399) {
+            val totalFinal = total + total2
+            this.hora = totalFinal/3600
+            this.minutos = (totalFinal % 3600) / 60
+            this.segundos = totalFinal % 60
+            return Tiempo(this.hora, this.minutos, this.segundos)
+        } else return null
     }
 
+    /**
+     * Resta las dos horas y devuelve la hora sumada o un nulo si se pasa de 00:00:00
+     * @param t :Tiempo tiempo que se va a restar
+     * @return Tiempo? devuelve un objeto Tiempo o un nulo
+     */
+    fun restar(t:Tiempo):Tiempo? {
+        val total = (this.hora * 3600) + (this.minutos * 60) + this.segundos
+        val total2 = (t.hora * 3600) + (t.minutos * 60) + t.segundos
 
+        if (total - total2 > 0) {
+            val totalFinal = total - total2
+            this.hora = totalFinal/3600
+            this.minutos = (totalFinal % 3600) / 60
+            this.segundos = totalFinal % 60
+            return Tiempo(this.hora, this.minutos, this.segundos)
+        }
+        else return null
+    }
 
+    /**
+     * Compara si la hora es mayor que la otra
+     * @param t :Tiempo tiempo que se va comparar
+     * @return Boolean si el tiempo es mayor devuelve true si no false
+     */
+    fun esMayorQue(t:Tiempo):Boolean {
+        return this.comparar(t) == 1
+    }
 
+    /**
+     * Compara si la hora es menor que la otra
+     * @param t :Tiempo tiempo que se va comparar
+     * @return Boolean si el tiempo es menor devuelve true si no false
+     */
+    fun esMenorQue(t:Tiempo) :Boolean {
+        return this.comparar(t) == -1
+    }
 }
